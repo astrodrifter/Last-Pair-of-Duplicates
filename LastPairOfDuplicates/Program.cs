1c84fcf39b8735febdaac5b151d5d07c9af2304d
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-19 09:07:21 +1000

    Working code
    Displays the index of the first of the last pair in an array.

--- LastPairOfDuplicates/Program.cs
+++ LastPairOfDuplicates/Program.cs
@@ -1,30 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+// displays index of the first of last pair
 namespace LastPairOfDuplicates {
     class Program {
         static void Main(string[] args) {
             int[] anArray = { 1, 9, 1, 9, 9, 5, 7, 7, 4 };
-            int index = LastPairOfDuplicates(anArray);
-
+            int index = LastPairOfDuplicates(anArray); // 6
+            DisplayResult(index);
+  
         }
         public static int LastPairOfDuplicates(int[] anArray) {
-            int index;
-            for (int i = anArray.Length; i > -1; i = i - 1) {
-                if (i == i + 1) {
-                    index = i;
-                    break;
-                    return index;
-                } else {
-                    return 
+            int index = 0;
+            for (int i = 1; i < anArray.Length; i++) {
+                if (anArray[i] == anArray[i - 1]) {
+                    index = i - 1;
                 }
-               
-                
-            }
-            
+            }          
+            return index;
+
+        }
+        public static void DisplayResult(int index) {
+            Console.WriteLine(index);
+            Console.ReadKey();
         }
     }
 }
