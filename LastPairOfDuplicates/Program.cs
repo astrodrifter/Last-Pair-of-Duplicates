de43de522229cc4f7cc3291db2464c4d2c6e9000
Dhruva O'Shea <dhruva.oshea@connect.qut.edu.au>
2016-06-18 23:56:17 +1000

    Attempting and Q3Bnum

---
+++ LastPairOfDuplicates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastPairOfDuplicates {
+    class Program {
+        static void Main(string[] args) {
+            int[] anArray = { 1, 9, 1, 9, 9, 5, 7, 7, 4 };
+            int index = LastPairOfDuplicates(anArray);
+
+        }
+        public static int LastPairOfDuplicates(int[] anArray) {
+            int index;
+            for (int i = anArray.Length; i > -1; i = i - 1) {
+                if (i == i + 1) {
+                    index = i;
+                    break;
+                    return index;
+                } else {
+                    return 
+                }
+               
+                
+            }
+            
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3Bnum {
+    class Program {
+        static void Main(string[] args) {
+            int[] myArray = { 200, 0, 100, -100, 0, 300 };
+            int num = 5;
+            bool found = false;
+            while (!found) {
+                if(myArray[num] ==0) {
+                    found = true;
+                }
+                num--;
+                Console.Write(num); //= 4
+                Console.ReadKey();
+            }
+        }
+    }
+}
